664a149fabf37663285db80e77a22b4bd7af90a1
pechsovann <chournp@gmail.com>
2020-06-25 10:06:09 +0700

    assignment1

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+</head>
+<body>
+      <!-- Nav bar -->
+      <nav>
+        <div class="logo">
+            <h4><a href="index.php">AwesomeShop</a></h4>
+        </div>
+        <ul class="nav-links">
+          <li style="margin-left: 50%;"><a href="#"><i class="fa fa-question-circle" aria-hidden="true"></i>NeedHelp</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
+          <li><button style="border-radius: 8px;"><a href="#">Join</a></button></li>
+        </ul>
+      </nav>
+
+      <!-- Background -- image-->
+      <div class="box">
+        <h2>Shop Your designer dresses</h2>
+              <p>ready to wear dereses taliored for you from online. Hery up while stock lasts.</p>
+         <!-- image - seacher - -->
+         
+          <div class="boxContainer">
+            
+            <table class="elementsContainer">
+              <tr>
+                <td>
+                  <input type="search" placeholder=" &nbsp;Search your products from here" class="search">
+                </td>
+                <td>
+                  <a href="#"><i class="fa fa-search" aria-hidden="true"></i></i></a>
+                </td>
+              </tr>
+            </table>
+          </div>
+      </div>    
+
+      <!-- table promotion-->
+      <div class="pricing-table">
+        <div class="pricing-card1">
+          <ul>
+            <li><strong><h3>Coupon savings</h3></strong></li>
+            <li>Up to 60% off everyday <img src="image/save.png" alt=""></li>
+            
+            <button class="order-btn">
+              <a href="#" > Shop Coupone</a></button>
+            
+          </ul>
+        </div>
+
+        <div class="pricing-card2">
+          <ul>
+            <li><strong><h3>Free Delivery</h3></strong></li>
+            <li>With & elected items <img src="image/delivery.png" alt=""></li> 
+            <button class="order-btn" ><a href="#"> Deliver Now </a></button>
+            
+          </ul>
+        </div>
+
+        <div class="pricing-card3">
+          <ul>
+            <li><strong><h3>GIFT Voucher</h3></strong></li>
+            <li> With personal care items <img src="image/gift.png" alt=""></li>
+            <button class="order-btn"><a href="#"> Gift Now</a></button>
+            
+          </ul>
+        </div>
+      </div>
+      <!-- catagory main-news -->
+      <div class="product">
+      <nav class="catagory">
+        <ul >
+          <li><a href="#Electronices">Electronics<img src="image/computer.png" alt=""></a></li><br>
+          <li><a href="#Hand-bags"> Hand-bags<img src="image/school.png" alt=""></a></li><br>         
+          <li><a href="#Wallet">Wallet<img src="image/wallet.png" alt=""></a></li><br>
+          <li><a href="#Clothes">Clothes <img src="image/clothe.png" alt=""></a></li>
+        </ul>
+        </nav>
+      <!-- card-products -->
+        <div>
+          <img src="" alt="">
+          <p></p>
+          <button></button>
+        </div>
+     
+      <!-- load more -->
+      </div>
+</body>
+</html>
